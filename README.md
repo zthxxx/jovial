baafd01da54b2271cb656f7630675af71592b9de
zthxxx <zthxxx.me@gmail.com>
2018-04-04 23:40:25 +0800

    fix: fix install with/without root permission

--- README.md
+++ README.md
@@ -14,14 +14,22 @@
 
 ## Install
 
+if u are login as **root**, pls use this command:
+
 ```bash
-curl -sSL git.io/jovial | zsh
+curl -sSL git.io/jovial | bash
+```
+
+otherwise, use this below:
+
+```bash
+sudo bash -c "`curl -sSL git.io/jovial`" -- $USER
 ```
 
 
 ## Author
 
-**SSlaunch.sh** © [zthxxx](https://github.com/zthxxx), Released under the **[MIT](./LICENSE)** License.<br>
+**SSlaunch.sh** © [zthxxx](https://github.com/zthxxx), Released under the **[MIT](./LICENSE)** License.
 
 > Blog [@zthxxx](https://blog.zthxxx.com) · GitHub [@zthxxx](https://github.com/zthxxx)
 
--- installer.zsh
+++ installer.zsh
@@ -1,16 +1,19 @@
 #!/usr/bin/env zsh
 
 install_zsh() {
-    if [ -z "${ZSH_VERSION:-}" ]; then
-        if command -v zsh 2> /dev/null; then 
-            chsh -s `command -v zsh`
+    # other ref: https://unix.stackexchange.com/questions/136423/making-zsh-default-shell-without-root-access?answertab=active#tab-top
+    local UNAME="$1"
+    if [ -z "${ZSH_VERSION}" ]; then
+        if command -v zsh > /dev/null; then 
+            chsh $UNAME -s `command -v zsh`
+            return 0
         fi
         echo "this theme base on zsh, trying to install it!" >&2
         if brew install zsh || \
             apt install -y zsh || \
             apt-get install -y zsh || \
             yum -y install zsh ; then
-            chsh -s `command -v zsh`
+            chsh $UNAME -s `command -v zsh`
         else
             echo "ERROR, plz install zsh manual."
             return 1
@@ -21,14 +24,19 @@ install_zsh() {
 install_ohmyzsh() {
     if [ -z "${ZSH:-}" -o -z "${ZSH_CUSTOM:-}" ]; then
         echo "this theme base on oh-my-zsh, now will install it!" >&2
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+        curl -fsSL http://install.ohmyz.sh | sh
     fi
 }
 
-(install_zsh && install_ohmyzsh) || exit 1
+(install_zsh "$1" && install_ohmyzsh) || exit 1
 
-local ZTHEME="jovial"
-local theme_path="github.com/zthxxx/${ZTHEME}/raw/master/${ZTHEME}.zsh-theme"
-local theme_file="${ZSH_CUSTOM:-"${HOME}/.oh-my-zsh/custom"}/${ZTHEME}.zsh-theme"
-curl -sSL "$theme_path" -o "$theme_file"
-sed  "-i''" "s/^ZSH_THEME=.*/ZSH_THEME=\"${ZTHEME}\"/g" ~/.zshrc
+install_theme() {
+    local ZTHEME="jovial"
+    local theme_path="github.com/zthxxx/${ZTHEME}/raw/master/${ZTHEME}.zsh-theme"
+    local theme_file="${ZSH_CUSTOM:-"${HOME}/.oh-my-zsh/custom"}/${ZTHEME}.zsh-theme"
+    curl -sSL "$theme_path" -o "$theme_file"
+    sed  "-i''" "s/^ZSH_THEME=.*/ZSH_THEME=\"${ZTHEME}\"/g" ~/.zshrc
+}
+
+install_theme
+
